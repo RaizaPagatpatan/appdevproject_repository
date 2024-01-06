43826dd0fd87b6abba132ee5854f1addf57ac473
Raiza Pagatpatan <rjimenezpagatpatan@gmail.com>
2024-01-06 23:43:19 +0800

    Vercel settings.py

--- eveappdev/eveappdev/wsgi.py
+++ eveappdev/eveappdev/wsgi.py
@@ -13,4 +13,8 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eveappdev.settings')
 
+
 application = get_wsgi_application()
+
+# vercel_app/wsgi.py
+app = get_wsgi_application()
